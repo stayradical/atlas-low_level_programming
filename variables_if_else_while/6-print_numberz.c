9ce2ce838d460435ee32d4bc4b5decd6b75ba196
stayradical <trinityjoy@atlasschool.com>
2024-05-20 21:12:31 +0000

    More numbers.

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - More numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
