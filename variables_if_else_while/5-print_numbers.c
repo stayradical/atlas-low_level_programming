7a7c79505d05f1454e18ead4a73677a185d08999
stayradical <trinityjoy@atlasschool.com>
2024-05-20 21:07:52 +0000

    Back to numbers.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Back to numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%i", n);
+
+	putchar('\n');
+
+	return (0);
+}
