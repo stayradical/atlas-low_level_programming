b9afa64dc554df9aff9442837468dde5f2025a28
stayradical <trinityjoy@atlasschool.com>
2024-05-20 19:24:57 +0000

    Alphabet game.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.>
+/**
+ * main - The alphabet game
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
