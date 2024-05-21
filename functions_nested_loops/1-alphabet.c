d95749831b19e6905ef45cb5365622b799faed75
stayradical <trinityjoy@atlasschool.com>
2024-05-21 01:14:48 +0000

    Alphabet again.

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ * Description: Prints the alphabet with _putchar
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
