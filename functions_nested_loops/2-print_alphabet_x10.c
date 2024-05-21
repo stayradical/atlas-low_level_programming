f154ef4a2e37668288ad3c87bf599262b7075b61
stayradical <trinityjoy@atlasschool.com>
2024-05-21 01:29:30 +0000

    Printing alphabet 10 times.

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Description: using _putchar
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,5 +2,14 @@
 #define main_h
 int _putchar(char c);
 void print_alphabet(void);
-
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int x);
+int print_last_digit(int x);
+void jack_bauer(void);
+void times_tables(void);
+int add (int a, int b);
+void print_to_98(int n);
 #endif
