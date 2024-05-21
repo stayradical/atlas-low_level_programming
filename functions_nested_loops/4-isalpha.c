a18bd3be22be17e9622774bf15d3d561c2562e7d
stayradical <trinityjoy@atlasschool.com>
2024-05-21 01:42:04 +0000

    Check for character.

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - check character.
+ * @c: character
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
