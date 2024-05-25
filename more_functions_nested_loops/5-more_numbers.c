d784ea61f6cd4f192b6f8f376972c04058c2cbb1
stayradical <trinity.joy@atlasschool.com>
2024-05-25 16:55:03 +0000

    Print 0-14, 10 times.

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-				_putchar((num / 10) + '0')
+				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
